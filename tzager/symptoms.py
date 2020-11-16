d0d07114af5a8a8f96b1e61dfed7a972b98958dd
antok33 <apostntok@gmail.com>
2020-11-16 12:12:27 +0200

    Adding symptoms procedure

---
+++ tzager/symptoms.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+
+def symptoms(password, concept_list, type_of_concepts):
+    concept_list = '|'.join(concept_list)
+    
+    response = requests.get('https://cloud.bolooba.com:25556/symptoms_data/' + password + '/' + concept_list + '/' + type_of_concepts)
+    if response.status_code == 200:
+        data = dict(response.json)
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
