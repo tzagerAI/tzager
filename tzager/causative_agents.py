cf7c11d22d41c5ad1b70a1f41c02dcf86bde9ab8
antok33 <apostntok@gmail.com>
2020-11-17 16:02:00 +0200

    add therapeutic and causative agents

---
+++ tzager/causative_agents.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+def get_data(password, concept_list):
+    concept_list = '|'.join(concept_list)
+    
+    response = requests.get('https://cloud.bolooba.com:25556/causative_data/' + password + '/' + concept_list)
+    if response.status_code == 200:
+        data = dict(response.json())
+        
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
