4feb4869f8cd01f867709b2a19d2424025426dfe
antok33 <apostntok@gmail.com>
2020-11-18 17:27:37 +0200

    Fix bug in why function

--- tzager/general_goal.py
+++ tzager/general_goal.py
@@ -5,7 +5,7 @@ def get_path(password, data, type_of_data):
     if type_of_data == 'list':
         data = '|'.join(data)
     
-    response = requests.get('https://cloud.bolooba.com:25556/general_goal/' + password + '/' + data + '/' + type_of_data)
+    response = requests.get('https://cloud.bolooba.com:25556/get_why/' + password + '/' + data + '/' + type_of_data)
     if response.status_code == 200:
         data = dict(response.json())
         
