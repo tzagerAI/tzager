cdc65ced0250edc112725d951a0465d73e6d0ee8
antok33 <apostntok@gmail.com>
2020-11-20 13:23:16 +0200

    Update set up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tzager", 
-    version="0.0.1.8.5",
+    version="0.0.1.8.6",
     author="tzager",
     author_email = "nikos@intoolab.com,a.ntokos@intoolab.com",
     description="A demo example package",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/tzagerAI/tzager",
     project_urls = {"Code": "https://github.com/tzagerAI/tzager"},
     packages=setuptools.find_packages(),
-    install_requires=["requests", "pdfminer", "pandas"],
+    install_requires=["requests", "pdfminer", "pandas", "networkx"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
