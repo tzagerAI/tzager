c3b35bad9d627dfabcc75d8137dd3b73ee1305ad
antok33 <apostntok@gmail.com>
2020-11-24 17:53:57 +0200

    Bayesian Network and Inference (enumeration)

--- tzager/bayesian.py
+++ tzager/bayesian.py
@@ -23,7 +23,7 @@ def create_network(variable_connections, probabilities, save_network_image=False
     if not nx.is_directed_acyclic_graph(G):
         print("Invalid Network ...\n", "Give a directed acyclic graph (DAG) ...")
         return ''
-    else:
+    else:x
         nodes = G.nodes()
         is_valid = validate_probability_table(G, nodes, probabilities)
 
@@ -38,20 +38,80 @@ def create_network(variable_connections, probabilities, save_network_image=False
 
 
 def validate_probability_table(G, nodes, probabilities_table):
-    invalid_nodes = []
-    for node in nodes:
-        parents = list(G.predecessors(node))
-        if not parents:
-            if not (node in probabilities_table and type(probabilities_table[node]) == float): 
-                invalid_nodes.append('No probability for ' + node + ' in probabilities data.')
+    errors = []
+    for node in probabilities_table:
+        sum_pr = 0
+        if type(probabilities_table[node]) is list:
+            for case in probabilities_table[node]:
+                value = case[1]
+                sum_pr += value
+            if sum_pr != 1:
+                errors.append('Error in probability distribution of variable: ' + node)
         else:
-            for p in parents:
-                if p not in list(probabilities_table[node].keys()):
-                    invalid_nodes.append('Missing probability for ' + node + '|' + p + '.')
-    if not invalid_nodes:
+            sum_pr = 0
+            for value in probabilities_table[node]:
+                sum_pr += probabilities_table[node][value]
+            if sum_pr != 1:
+                errors.append('Error in probability distribution of variable: ' + node)
+    if not errors:
         return True
     else:
-        for error in invalid_nodes:
-            print(error)
+        for er in errors:
+            print(er)
         return False
+
+def full_joint_probability(assignment, network):
+    assignment_pr = 1
+    G = network['graph']
+    CPT = network['probabilities']
+    nodes = G.nodes
+    for n in nodes:
+        parents = list(G.predecessors(n))
+        if type(CPT[n]) is list:
+            case_of_assignment = {}
+            for p in parents:
+                case_of_assignment[p] = assignment[p]
+            for case in CPT[n]:
+                if case[0] == case_of_assignment:
+                    if assignment[n] == 'T':
+                        assignment_pr *= case[1]
+                    else:
+                        assignment_pr *= (1 - case[1])
+                    break
+        else:
+            assignment_pr *= CPT[n][assignment[n]]
+    return {'Pr': assignment_pr}
+
+def inference(query, network, algorithm='enumeration'):
+    from itertools import product
+    possible_values = ["T", "F"]
+    G = network['graph']
+    nodes = G.nodes
+    query_variables = query['variable']
+    evidence_variables = query['given']
+    total_probability = 0
+    if algorithm == 'enumeration':
+        hidden_variables = []
+        for n in nodes:
+            if n not in query_variables and n not in evidence_variables:
+                hidden_variables.append(n)
+        print('Query Variables:', list(query_variables.keys()))
+        print('Evidence Variables:', list(evidence_variables.keys()))
+        print('Hidden Variables:', hidden_variables)
+        print()
+    
+    hidden_variables_dict = {}
+    for i,h in enumerate(hidden_variables):
+        hidden_variables_dict[i] = h
+
+    assignment = {**query_variables, **evidence_variables}
+    
+    hidden_variables_possible_assignments = list(product(possible_values, repeat=len(hidden_variables)))
+    for pair in hidden_variables_possible_assignments:
+        for i, value in enumerate(pair):
+            assignment[hidden_variables_dict[i]] = value
+        total_probability += full_joint_probability(assignment, network)['Pr']
+    return {'Inference Pr': total_probability}
+
+
 
