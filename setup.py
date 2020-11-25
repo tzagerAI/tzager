ec0748f52887bb98ccccfbc61d772ba9a80108af
antok33 <apostntok@gmail.com>
2020-11-25 09:47:02 +0200

    Update Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tzager", 
-    version="0.0.1.9.4",
+    version="0.0.1.9.5",
     author="tzager",
     author_email = "nikos@intoolab.com,a.ntokos@intoolab.com",
     description="A demo example package",
