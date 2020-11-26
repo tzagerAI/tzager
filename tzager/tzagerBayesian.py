192b06edc92ec39b106094eb92d9645edb68e54e
antok33 <apostntok@gmail.com>
2020-11-26 14:11:15 +0200

    Add conditional probabilities function

--- tzager/tzagerBayesian.py
+++ tzager/tzagerBayesian.py
@@ -2,58 +2,56 @@ import requests
 import json
 from collections import Counter
 
-def build_network(data, variables='None'):
+
+def values_for_variables(data):
+    all_possible_variables = []
+
+    for id in data:
+        for variable in data[id]:
+            value = data[id][variable]
+            all_possible_variables.append(variable + '=' + str(value))
+    
+    all_possible_variables = list(set(all_possible_variables))
+    return all_possible_variables
+
+
+def conditional_prob(data, q):
     number_of_samples = len(data)
-    network = {}
-    if variables == 'None':
-        for id in data:
-            connections = []
-            for c in data[id]:
-                value = data[id][c]
-                connections.append(c + "|" + str(value))
-            for var in connections:
-                try:
-                    network[var] += connections
-                except KeyError:
-                    network[var] = []
-                    network[var] += connections
-    else:
-        for id in data:
-            connections = []
-            for c in data[id]:
-                if c in variables:
-                    value = data[id][c]
-                    connections.append(c + "|" + str(value))
-            for var in connections:
-                try:
-                    network[var] += connections
-                except KeyError:
-                    network[var] = []
-                    network[var] += connections
-    
-    for var in network:
-        frequencies = Counter(network[var])
-        frequencies = dict(sorted(frequencies.items(), key=lambda item: item[1], reverse=True))
-        network[var] = frequencies
-    
-    for var in network:
-        for conn in network[var]:
-            network[var][conn] = round(network[var][conn] / number_of_samples, 3)
-    return network
-
-
-def single_inference(network, q):
+    variables_join = {}
     query_variable = q['query_variable'] 
     evidence_variable = q['evidence_variables']
-    total_pr = 1
+    if evidence_variable:
+        print('Calculating:', "Pr[" + query_variable + ' | ' + ','.join(evidence_variable) + ']')
+    else:
+        print('Calculating:', "Pr[" + query_variable + ']')
+    all_possible_variables = []
 
-    try:
-        if not evidence_variable:
-            total_pr *= network[query_variable][query_variable]
+    for id in data:
+        instance = []
+        for variable in data[id]:
+            value = data[id][variable]
+            instance.append(variable + '=' + str(value))
+            all_possible_variables.append(variable + '=' + str(value))
+        variables_join[id] = instance
+    
+    all_possible_variables = list(set(all_possible_variables))
+
+    if not evidence_variable:
+        query_hits = 0
+        for id in variables_join:
+            if query_variable in variables_join[id]:
+                query_hits += 1
+        total_pr = round(query_hits/number_of_samples, 3)
+    else:
+        evidence_query_join = 0
+        evidence_join = 0
+        for id in variables_join:
+            if set(evidence_variable) <= set(variables_join[id]):
+                evidence_join += 1
+            if set(evidence_variable + [query_variable]) <= set(variables_join[id]):
+                evidence_query_join += 1
+        if evidence_join != 0:
+            total_pr = round(evidence_query_join/evidence_join, 3)
         else:
-            total_pr *= (network[query_variable][evidence_variable])/network[evidence_variable][evidence_variable]
-    except KeyError:
-        total_pr = 0
-
+            total_pr = 0
     return total_pr
-
