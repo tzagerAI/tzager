547d6b6298832deffa36616419f5736019e8f2af
antok33 <apostntok@gmail.com>
2020-12-10 18:15:00 +0200

    Add scenarios analysis in knowledge modeling

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tzager", 
-    version="0.0.3.1.3",
+    version="0.0.3.1.4",
     author="tzager",
     author_email = "nikos@intoolab.com,a.ntokos@intoolab.com",
     description="A demo example package",
--- tzager/knowledge_modeling.py
+++ tzager/knowledge_modeling.py
@@ -20,26 +20,13 @@ def get_literature(password, paths, num_papers=50):
         data = dict(data)
     return data
 
-def fill_fractals(password, paths, topn=5, report=True):
-    response = requests.post('https://cloud.bolooba.com:25556/fill_fractals/' + password + '/' + str(topn), json=json.dumps(paths))
+def fill_fractals(password, paths):
+    response = requests.post('https://cloud.bolooba.com:25556/fill_fractals/' + password , json=json.dumps(paths))
     if response.status_code == 200:
         data = dict(response.json())
-        if report:
-            print()
-            print('BODY PATH')
-            for categ in data['Body']:
-                print(categ, ':', data['Body'][categ])
-            print('\n---------------------------------------------\n')
-            print('DISEASE PATH')
-            for categ in data['Disease']:
-                print(categ, ':', data['Disease'][categ])
-            print('\n---------------------------------------------\n')
-            print('THERAPY PATH')
-            for categ in data['Therapy']:
-                print(categ, ':', data['Therapy'][categ])
     else:
         data = {'error': response.status_code}
         data = dict(data)
+    
+    
     return data
-
-
