46e2bd2a8ba865c9314ce6fb88ca67c79368748c
antok33 <apostntok@gmail.com>
2020-12-14 18:08:42 +0200

    correct endpoint url

--- tzager/adverse_effects.py
+++ tzager/adverse_effects.py
@@ -5,7 +5,7 @@ import requests
 def get_data(password, concept_list, filters='None'):
 
     data_dict = {'concept_list': concept_list, 'filters': filters}
-    response = requests.get('http://127.0.0.1:5000/adverse_effects/' + password, json=json.dumps(data_dict))
+    response = requests.get('https://cloud.bolooba.com:25556/adverse_effects/' + password, json=json.dumps(data_dict))
     if response.status_code == 200:
         data = dict(response.json())
         
