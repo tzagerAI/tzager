f7d394dda127c509f2b214ba29da37486f031d15
antok33 <apostntok@gmail.com>
2020-12-18 14:45:00 +0200

    train feature

---
+++ tzager/train.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+def get_data(password, new_concept, based_on, data_to_get):
+
+    data_dict = {'new_concept': new_concept, 'based_on': based_on, 'data_to_get': data_to_get}
+    response = requests.get('https://cloud.bolooba.com:25556/train/' + password, json=json.dumps(data_dict))
+    if response.status_code == 200:
+        data = dict(response.json())        
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
+
+password = '$biomedicine$'
+new_concept = 'neovagina-intestine'
+based_on = 'Intestines'
+data_to_get = ['diseases', 'organisms']
+data = get_data(password, new_concept, based_on, data_to_get)
