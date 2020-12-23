ed6904b656c03478a817c8ad35b7bf3a3bf6545e
antok33 <apostntok@gmail.com>
2020-12-23 13:35:32 +0200

    Attention feature

---
+++ tzager/attention.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+
+def get_data(password, question, focused_concepts):
+
+    data_dict = {'question': question, 'focused_concepts': focused_concepts}
+    response = requests.get('https://cloud.bolooba.com:25556/attention/' + password, json=json.dumps(data_dict))
+    if response.status_code == 200:
+        data = dict(response.json())
+        
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
