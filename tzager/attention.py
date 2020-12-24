1aff81ac627cd962301c5e467ed0a5aca6b92105
antok33 <apostntok@gmail.com>
2020-12-24 11:04:31 +0200

    Attention add data to get

--- tzager/attention.py
+++ tzager/attention.py
@@ -2,9 +2,9 @@ import requests
 import json
 
 
-def get_data(password, question, focused_concepts):
+def get_data(password, question, focused_concepts, data_to_get):
 
-    data_dict = {'question': question, 'focused_concepts': focused_concepts}
+    data_dict = {'question': question, 'focused_concepts': focused_concepts, 'data_to_get': data_to_get}
     response = requests.get('https://cloud.bolooba.com:25556/attention/' + password, json=json.dumps(data_dict))
     if response.status_code == 200:
         data = dict(response.json())
@@ -13,3 +13,4 @@ def get_data(password, question, focused_concepts):
         data = {'error': response.status_code}
         data = dict(data)
     return data
+
