7402cbfc80fc338f6a879f123c28b1b09c1a1ef4
antok33 <apostntok@gmail.com>
2021-02-04 17:47:19 +0200

    Update brainstorm

--- tzager/brainstorm.py
+++ tzager/brainstorm.py
@@ -1,8 +1,8 @@
 import json
 import requests
 
-def question(password, discussion_id, query):
-    response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id}))
+def question(password, discussion_id, query, question_id=None):
+    response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id, 'question_id': question_id}))
     # response = requests.post('http://127.0.0.1:5000/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id}))
     if response.status_code == 200:
         data = dict(response.json())
@@ -10,7 +10,7 @@ def question(password, discussion_id, query):
         data = {'error': response.status_code}
         data = dict(data)
 
-def simulate(password, discussion_id, question_id, query):
+def simulate(password, discussion_id, query, question_id=None):
     response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/simulate/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id, 'question_id': question_id}))
     # response = requests.post('http://127.0.0.1:5000/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id}))
     if response.status_code == 200:
