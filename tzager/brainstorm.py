1644f7031e97886d6cd057de4a49f6dce111212b
antok33 <apostntok@gmail.com>
2021-02-05 13:34:56 +0200

    Add dig deeper feature  in brainstorm funtion

--- tzager/brainstorm.py
+++ tzager/brainstorm.py
@@ -27,3 +27,12 @@ def get_discussion_info(password, discussion_id):
     else:
         data = {'error': response.status_code}
         data = dict(data)
+
+def dig_deeper(password, discussion_id, question_id, node):
+    response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/dig_deeper/' + password, json=json.dumps({'discussion_id': discussion_id, 'question_id': question_id, 'node': node}))
+    # response = requests.post('http://127.0.0.1:5000/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
