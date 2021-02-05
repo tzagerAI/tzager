e106499255f234362b0d2166c442585b46a9e145
antok33 <apostntok@gmail.com>
2021-02-05 14:08:14 +0200

    Update 5-2

--- tzager/brainstorm.py
+++ tzager/brainstorm.py
@@ -21,12 +21,13 @@ def simulate(password, discussion_id, query, question_id=None):
 
 def get_discussion_info(password, discussion_id):
     response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/get_discussion_info/' + password, json=json.dumps({'discussion_id': discussion_id}))
-    # response = requests.post('http://127.0.0.1:5000/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id}))
+    # response = requests.post('http://127.0.0.1:5000/get_discussion_info/' + password, json=json.dumps({'discussion_id': discussion_id}))
     if response.status_code == 200:
         data = dict(response.json())
     else:
         data = {'error': response.status_code}
         data = dict(data)
+    return data
 
 def dig_deeper(password, discussion_id, question_id, node):
     response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/dig_deeper/' + password, json=json.dumps({'discussion_id': discussion_id, 'question_id': question_id, 'node': node}))
