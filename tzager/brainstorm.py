737b7c04c557b3097918a1b556220b0eb32eadb1
antok33 <apostntok@gmail.com>
2021-02-08 14:13:56 +0200

    Get Paper in Brainstorm

--- tzager/brainstorm.py
+++ tzager/brainstorm.py
@@ -37,3 +37,13 @@ def dig_deeper(password, discussion_id, question_id, node):
     else:
         data = {'error': response.status_code}
         data = dict(data)
+
+def get_papers(password, discussion_id, question_id):
+    response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/get_paper/' + password, json=json.dumps({'discussion_id': discussion_id, 'question_id': question_id}))
+    # response = requests.post('http://127.0.0.1:5000/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+
