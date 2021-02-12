ab41cdefe20ec07f181f3a74b4fdbcf334e74c6d
antok33 <apostntok@gmail.com>
2021-02-12 10:44:41 +0200

    Add question_library

--- tzager/brainstorm.py
+++ tzager/brainstorm.py
@@ -3,7 +3,17 @@ import requests
 
 def question(password, discussion_id, query, question_id=None):
     response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id, 'question_id': question_id}))
-    # response = requests.post('http://127.0.0.1:5000/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id}))
+    # response = requests.post('http://127.0.0.1:5000/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id, 'question_id': question_id}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
+
+def question_library(password, discussion_id, query, question_id=None):
+    response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/question_library/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id, 'question_id': question_id}))
+    # response = requests.post('http://127.0.0.1:5000/question/' + password, json=json.dumps({'query':query, 'discussion_id': discussion_id, 'question_id': question_id}))
     if response.status_code == 200:
         data = dict(response.json())
     else:
@@ -50,4 +60,3 @@ def get_papers(password, discussion_id, question_id):
         data = {'error': response.status_code}
         data = dict(data)
     return data
-
