75e1111fa0a712d51c40ce286026275d075af6c2
antok33 <apostntok@gmail.com>
2021-02-22 14:03:02 +0200

    complemetary_abstracts and scientific_analysis in input

--- tzager/input.py
+++ tzager/input.py
@@ -9,4 +9,22 @@ def abstract_scopes(password, abstracts):
         data = {'error': response.status_code}
         data = dict(data)
     return data
-    
+
+
+def complemetary_abstracts(password, data_for_complemetary_abstracts):
+    response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/complemetary_abstracts/' + password, json=json.dumps({'data_for_complemetary_abstracts': data_for_complemetary_abstracts}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
+
+def scientific_analysis(password, abstracts, data_for_scientific_analysis, complemetary_data):
+    response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/scientific_analysis_abstracts/' + password, json=json.dumps({'data_for_scientific_analysis': data_for_scientific_analysis, 'abstracts': abstracts, 'complemetary_data': complemetary_data}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
