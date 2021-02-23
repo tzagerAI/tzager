71eae37c5f5510f7ce4fcc5e3e2379a39ebfba48
antok33 <apostntok@gmail.com>
2021-02-23 16:12:21 +0200

    Add abstract query

--- tzager/input_abstracts.py
+++ tzager/input_abstracts.py
@@ -48,9 +48,17 @@ def abstract_augment_scopes(password, abstracts, data_for_scientific_analysis, c
         data = dict(data)
     return data
 
-
 def question_hbn(password, question):
     response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/question_hbn/' + password, json=json.dumps({'question_text': question}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
+
+def question_abstracts(password, question, abstracts):
+    response = requests.post('http://tzagerlib1-env.eba-wjp8tqpj.eu-west-2.elasticbeanstalk.com/question_abstracts/' + password, json=json.dumps({'question_text': question, 'abstracts': abstracts}))
     if response.status_code == 200:
         data = dict(response.json())
     else:
