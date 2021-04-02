cbddf89c691c2d67691aaf60325138e472e548e7
antok33 <apostntok@gmail.com>
2021-04-02 16:17:47 +0300

    Add get connections

---
+++ tzager/get_connections.py
@@ -0,0 +1,56 @@
+import json
+import requests
+
+def anatomies(password, concepts_list, pmids=[], filters=[]):
+    response = requests.post('https://intoolab.ai/get_anatomies/' + password, json=json.dumps({'concepts_list':concepts_list, 'pmids': pmids, 'filters': filters}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
+
+def diseases(password, concepts_list, pmids=[], filters=[]):
+    response = requests.post('https://intoolab.ai/get_diseases/' + password, json=json.dumps({'concepts_list':concepts_list, 'pmids': pmids, 'filters': filters}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
+
+def symptoms(password, concepts_list, pmids=[], filters=[]):
+    response = requests.post('https://intoolab.ai/get_symptoms/' + password, json=json.dumps({'concepts_list':concepts_list, 'pmids': pmids, 'filters': filters}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
+
+def organisms(password, concepts_list, pmids=[], filters=[]):
+    response = requests.post('https://intoolab.ai/get_organisms/' + password, json=json.dumps({'concepts_list':concepts_list, 'pmids': pmids, 'filters': filters}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
+
+def therapies(password, concepts_list, pmids=[], filters=[]):
+    response = requests.post('https://intoolab.ai/get_therapies/' + password, json=json.dumps({'concepts_list':concepts_list, 'pmids': pmids, 'filters': filters}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
+
+def phenomena(password, concepts_list, pmids=[], filters=[]):
+    response = requests.post('https://intoolab.ai/get_phenomena/' + password, json=json.dumps({'concepts_list':concepts_list, 'pmids': pmids, 'filters': filters}))
+    if response.status_code == 200:
+        data = dict(response.json())
+    else:
+        data = {'error': response.status_code}
+        data = dict(data)
+    return data
